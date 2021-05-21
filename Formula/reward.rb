fa180ffe796278a24eb3e4daebb98d40b78e47a1
Janos Miko <mixe3y@gmail.com>
2021-05-21 15:07:34 +0200

    Brew formula update for reward version v0.2.0-beta

--- Formula/reward.rb
+++ Formula/reward.rb
@@ -11,11 +11,11 @@ class Reward < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/rewardenv/reward/releases/download/v0.2.0-beta/reward_Darwin_x86_64.tar.gz"
-    sha256 "f63e82eb14216534811a0fff5953a584285ec20d9ce22c2c58a77b6fca702b1c"
+    sha256 "98d258a44b5caab27ed56bbbb01e75ebda53a92f0476876ef45354ecfde2d28f"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/rewardenv/reward/releases/download/v0.2.0-beta/reward_Linux_x86_64.tar.gz"
-    sha256 "e2f501d3f6cb33242c68e28f3be576dbf361e303a8890677d236f34d7bf67c5b"
+    sha256 "d003312a19e784b2c7372023bf42e299b42de8d8b33bde852a8eadea5bb0c436"
   end
 
   depends_on "docker" => :optional
