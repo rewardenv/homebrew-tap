940af0a66d51fef555b95b2f9aec919bdd702e8c
Janos Miko <mixe3y@gmail.com>
2021-05-21 13:56:19 +0200

    Brew formula update for reward version v0.2.0-beta

--- Formula/reward.rb
+++ Formula/reward.rb
@@ -5,17 +5,17 @@
 class Reward < Formula
   desc "Reward is CLI utility for orchestrating Docker based development environments."
   homepage ""
-  version "0.1.27-beta"
+  version "0.2.0-beta"
   license "MIT"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/rewardenv/reward/releases/download/v0.1.27-beta/reward_Darwin_x86_64.tar.gz"
-    sha256 "50203f82537747f6b2b64d41fe0abe0953136b3295fb81fe35f8b587e8ca1245"
+    url "https://github.com/rewardenv/reward/releases/download/v0.2.0-beta/reward_Darwin_x86_64.tar.gz"
+    sha256 "f63e82eb14216534811a0fff5953a584285ec20d9ce22c2c58a77b6fca702b1c"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/rewardenv/reward/releases/download/v0.1.27-beta/reward_Linux_x86_64.tar.gz"
-    sha256 "052c4eca46fc49fc5abb6a459a8c4b82578d2749d3d801cbbe2b76f468d3cf26"
+    url "https://github.com/rewardenv/reward/releases/download/v0.2.0-beta/reward_Linux_x86_64.tar.gz"
+    sha256 "e2f501d3f6cb33242c68e28f3be576dbf361e303a8890677d236f34d7bf67c5b"
   end
 
   depends_on "docker" => :optional
