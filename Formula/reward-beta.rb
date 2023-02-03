4799899237fd96b8acab5e4b715bb37d1bb3f76d
Janos Miko <info@janosmiko.com>
2023-02-03 18:32:35 +0100

    Brew formula update for reward version v0.4.0-beta3

--- Formula/reward-beta.rb
+++ Formula/reward-beta.rb
@@ -14,7 +14,7 @@ class RewardBeta < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/rewardenv/reward/releases/download/v0.4.0-beta3/reward_Darwin_arm64.tar.gz"
-      sha256 "105cd6359e7230abca7e279bac32a8ec42980aae4867b7d6daba5e27405ab9a2"
+      sha256 "928d691e5fcaa65e1960bd49784d9f2b5d2f43bc64e7017eac0930bb4bcbc6c2"
 
       def install
         bin.install "reward"
@@ -22,7 +22,7 @@ class RewardBeta < Formula
     end
     if Hardware::CPU.intel?
       url "https://github.com/rewardenv/reward/releases/download/v0.4.0-beta3/reward_Darwin_x86_64.tar.gz"
-      sha256 "73c6108e7f573bc2825b4f30a0dc95b634076102236ff81b680da62c1a360f9c"
+      sha256 "374c1fb6e500bdf1a921b79822888895d70981f4abade5865d8a2f9875427b75"
 
       def install
         bin.install "reward"
@@ -33,7 +33,7 @@ class RewardBeta < Formula
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/rewardenv/reward/releases/download/v0.4.0-beta3/reward_Linux_aarch64.tar.gz"
-      sha256 "b06b5f1f4b35f3a91b546372017703fa793a09348d9d5d731540cb36e28ebe7d"
+      sha256 "11f3e07d7fd6c083c5e0448c9aed48ae333bd6a8bb681729522cecacd17b70ae"
 
       def install
         bin.install "reward"
@@ -41,7 +41,7 @@ class RewardBeta < Formula
     end
     if Hardware::CPU.intel?
       url "https://github.com/rewardenv/reward/releases/download/v0.4.0-beta3/reward_Linux_x86_64.tar.gz"
-      sha256 "1b3ef3ebe95ca1d3f3415aa6082fa37b4ae933da7ac9074a25c06f444a7b58a5"
+      sha256 "4caf95a69959fa0dce41df1e46917eae65fdffdc43887e053b3b5ffc746bcfb1"
 
       def install
         bin.install "reward"
